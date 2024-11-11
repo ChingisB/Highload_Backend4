71fade1ee383c411a5cf0875b898452f3224cd71
chingis <chingis.bagdatov@gmail.com>
2024-11-11 20:32:10 +0500

    add registration and login

--- tasks/main/urls.py
+++ tasks/main/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import send_email_view
+from .views import send_email_view, register
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('send-email/', send_email_view, name='send_email'),
+    path("register/", register, name="register"),
+    path("login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]
--- tasks/main/views.py
+++ tasks/main/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 from .forms import EmailForm
 from .tasks import send_email_task
+
 
 def send_email_view(request):
     if request.method == 'POST':
@@ -14,3 +17,18 @@ def send_email_view(request):
     else:
         form = EmailForm()
     return render(request, 'send_email.html', {'form': form})
+
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registration successful.")
+            return redirect("home")
+        else:
+            messages.error(request, "Unsuccessful registration. Invalid information.")
+    else:
+        form = UserCreationForm()
+    return render(request, "register.html", {"form": form})
